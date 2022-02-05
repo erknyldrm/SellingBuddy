9b28ee6580e475be5e3148560751f44d01ed54b1
erknyldrm <erkan-84@hotmail.com>
2022-02-05 14:58:24 +0300

    UnitTest implemented.

---
+++ EventBus.UnitTest/Events/Events/OrderCreatedIntegrationEvent.cs
@@ -0,0 +1,14 @@
+﻿using EventBus.Base.Events;
+
+namespace EventBus.UnitTest.Events.Events
+{
+    internal class OrderCreatedIntegrationEvent : IntegrationEvent
+    {
+        public int Id { get; set; }
+
+        public OrderCreatedIntegrationEvent(int id)
+        {
+            Id = id;
+        }
+    }
+}
